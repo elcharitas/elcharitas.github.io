7177e57fa7fdc42f3ff488a0ce99c8ef29dd0e89
elcharitas <jonathanirhodia@gmail.com>
2020-11-12 19:42:26 +0100

    Add support for Development Tools

---
+++ _src/ts/app.ts
@@ -0,0 +1,56 @@
+var hcb_user = {
+    comments_header: 'Leave a Reply.',
+    name_label: 'Your Name:',
+    content_label: 'Your reply...',
+    submit: 'Submit',
+    logout_link: 'Logout',
+    admin_link: '  ',
+    no_comments_msg: 'No replies yet!',
+    add: 'Reply Post',
+    again: 'New Reply',
+    rss: ' ',
+    said: 'said:',
+    prev_page: 'Older',
+    next_page: 'Newer',
+    showing: 'Showing',
+    to: 'to',
+    website_label: 'Your Website (optional)',
+    email_label: 'Email Address',
+    anonymous: 'Stay Anonymous?',
+    mod_label: 'Super User',
+    subscribe: 'Email Notifications?',
+    add_image: '+',
+    are_you_sure: 'Thanks for helping. Are you sure this is Spammy?',
+    reply: 'Comment',
+    flag: 'Flag',
+    like: 'Like',
+    days_ago: 'days ago',
+    hours_ago: 'hrs ago',
+    minutes_ago: 'mins ago',
+    within_the_last_minute: 'just now',
+    msg_thankyou: 'Thank you for replying!',
+    err_bad_html: 'Your reply contained bad html.',
+    err_bad_email: 'Please enter a valid email address.',
+    err_too_frequent: 'You must wait a few seconds between posting replies.',
+    err_comment_empty: 'Your reply was not posted because it was empty!',
+    err_denied: 'Your reply was not accepted.',
+    err_unknown: 'Your reply was blocked for unknown reasons, please report this.',
+    err_spam: 'Your reply was detected as spam.',
+    err_blocked: 'Your reply was blocked by site policy.',
+    MAX_CHARS: 8192,
+    PAGE: "https://wcodr.mybb.us/pages/gb",
+    USER: {
+        name: localStorage
+    },
+    ON_COMMENT: function() {
+
+    },
+    onload: function() {
+
+    },
+    RELATIVE_DATES: true
+};
+
+var src = document.createElement("script");
+src.src = "https://www.htmlcommentbox.com/jread?opts=2045&page=" + hcb_user.PAGE;
+document.getElementsByTagName("head")[0].appendChild(src);
